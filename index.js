c0e67d13e5f74ef5cdc921fe4a8887a844513c90
samandargofurov <samandargofurov236@gmail.com>
2024-01-23 19:42:27 +0500

    6 lesson

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1-masala
+// 
+// let n = +prompt("raqam kiriting: ");
+// let result = "";
+
+// switch (n) {
+//   case 1:
+//     result = "Yanvar";
+//     break;
+//   case 2:
+//     result = "Fevral";
+//     break;
+//   case 3:
+//     result = "Mart";
+//     break;
+//   case 4:
+//     result = "Aprel";
+//     break;
+//   case 5:
+//     result = "May";
+//     break;
+//   case 6:
+//     result = "Iyun";
+//     break;
+//   case 7:
+//     result = "Iyul";
+//     break;
+//   case 8:
+//     result = "Avgust";
+//     break;
+//   case 9:
+//     result = "Sentabr";
+//     break;
+//   case 10:
+//     result = "Oktabr";
+//     break;
+//   case 11:
+//     result = "Noyabr";
+//     break;
+//   case 12:
+//     result = "Dekabr";
+//     break;
+
+//   default:
+//     result = "bunday oy mavjud emas";
+//     break;
+// }
+
+// console.log(result);
+
+// 2-masala
+// 
+let n = 5;
+let sum = 0;
+
+for (let i = 0; i <= n; i++){
+  
+}
